51edc9f4f261e97ef85005a9b73b5909c982319c
erick <erick.zeiler@student.hs-rm.de>
2025-03-03 22:07:05 +0100

    ekkodale rest weather api

---
+++ Models/WeatherItem.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherAPI.Models
+{
+    public class WeatherItem
+    {
+        public required string Id { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float WindSpeed { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using WeatherAPI.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var neo4jConfig = builder.Configuration.GetSection("Neo4j");
+builder.Services.AddSingleton(new Neo4jService(
+    neo4jConfig["Uri"],
+    neo4jConfig["Username"],
+    neo4jConfig["Password"]
+));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
