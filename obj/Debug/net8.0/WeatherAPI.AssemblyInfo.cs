82045e875898507123ca65c6263861512de6a6bd
Erickk0 <erick.zeiler@student.hs-rm.de>
2025-03-05 23:01:58 +0100

    .

--- obj/Debug/net8.0/WeatherAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32cb92d607d18bc01bda3c32b38ae66b59a4b847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444da2b30b14583da663067280a7663092eef91d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
